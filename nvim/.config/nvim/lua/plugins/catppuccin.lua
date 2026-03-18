fdf2c359bf1d0f6aa39df3ee81841bf7e815d9f7
shakedown-street <pricejordan@outlook.com>
2026-03-18 16:29:01 -0600

    Start nvim plugins

---
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,12 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  config = function()
+    -- Choose your flavor: latte, frappe, macchiato, mocha
+    require("catppuccin").setup({
+      flavour = "macchiato",
+    })
+    -- Activate the colorscheme
+    vim.cmd("colorscheme catppuccin")
+  end
+}
