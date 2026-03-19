bce62ea399ed37d623bca298909ebea7eacbdd51
shakedown-street <pricejordan@outlook.com>
2026-03-18 23:12:35 -0600

    Start setting up lsp, mason and gitsigns

---
+++ nvim/.config/nvim/lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,13 @@
+return   {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "pyright",
+          "tsserver",
+        },
+      })
+    end,
+  }
---
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,33 @@
+return   {
+    "neovim/nvim-lspconfig",
+    dependencies = { "mason-lspconfig.nvim" },
+    config = function()
+      local lspconfig = require("lspconfig")
+
+      -- capabilities (optional but recommended if you add cmp later)
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+      -- mason-lspconfig hook
+      require("mason-lspconfig").setup_handlers({
+        function(server_name)
+          lspconfig[server_name].setup({
+            capabilities = capabilities,
+          })
+        end,
+
+        -- Example override for lua_ls
+        -- ["lua_ls"] = function()
+        --   lspconfig.lua_ls.setup({
+        --     capabilities = capabilities,
+        --     settings = {
+        --       Lua = {
+        --         diagnostics = {
+        --           globals = { "vim" },
+        --         },
+        --       },
+        --     },
+        --   })
+        -- end,
+      })
+    end,
+  }
