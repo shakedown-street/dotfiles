dc654d5c24a91010a5824ebe58afc17277da1f43
shakedown-street <pricejordan@outlook.com>
2026-03-19 17:04:39 -0600

    Configure blink

---
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,11 @@
+return {
+  "saghen/blink.cmp",
+  -- optional: provides snippets for the snippet source
+  dependencies = { "rafamadriz/friendly-snippets" },
+  version = "1.*",
+  opts = {
+    -- keymap = {
+    --   preset = "super-tab",
+    -- },
+  },
+}
