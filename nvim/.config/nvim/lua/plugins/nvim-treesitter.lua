e7792c4204cdb7be3bd9f005046f2b3b611ff5b5
shakedown-street <pricejordan@outlook.com>
2026-03-22 15:01:22 -0600

    Copy kickstart.nvim treesitter config

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,8 +1,9 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  lazy = false,
   build = ":TSUpdate",
   config = function()
-    local filetypes = {
+    local parsers = {
       "bash",
       "css",
       "dockerfile",
@@ -20,11 +21,26 @@ return {
       "yaml",
       "zsh",
     }
-    require("nvim-treesitter").install(filetypes)
+    require("nvim-treesitter").install(parsers)
+
     vim.api.nvim_create_autocmd("FileType", {
-      pattern = filetypes,
-      callback = function()
-        vim.treesitter.start()
+      pattern = parsers,
+      callback = function(args)
+        local buf, filetype = args.buf, args.match
+
+        local language = vim.treesitter.language.get_lang(filetype)
+        if not language then
+          return
+        end
+
+        -- check if parser exists and load it
+        if not vim.treesitter.language.add(language) then
+          return
+        end
+        -- enables syntax highlighting and other treesitter features
+        vim.treesitter.start(buf, language)
+
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
       end,
     })
   end,
