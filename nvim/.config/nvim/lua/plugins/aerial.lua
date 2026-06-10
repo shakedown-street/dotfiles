18b83ec9dff06c50804064d3927737bda83abe56
shakedown-street <pricejordan@outlook.com>
2026-06-10 15:35:37 -0600

    Add aerial for symbol navigation

---
+++ nvim/.config/nvim/lua/plugins/aerial.lua
@@ -0,0 +1,14 @@
+return {
+  "stevearc/aerial.nvim",
+  opts = {},
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("aerial").setup({
+      close_on_select = true,
+    })
+    vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle<CR>")
+  end,
+}
