173e8458129fd6a7562b07d542ae6fcf978020e8
shakedown-street <pricejordan@outlook.com>
2026-08-23 21:17:42 -0600

    Neovim line wrapping improvements

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -20,6 +20,10 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.autoindent = true
 
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.breakindent = true
+
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
